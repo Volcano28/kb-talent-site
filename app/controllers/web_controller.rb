149b8d869a8951887d23556983e86e13ab62e6a6
Professor Raghunath <raghunat@fredonia.edu>
2015-03-02 20:43:05 -0500

    Updating registration views for errors

--- app/controllers/web_controller.rb
+++ app/controllers/web_controller.rb
@@ -9,9 +9,23 @@ class WebController < ApplicationController
   end
 
   def create_registration
+    @errors = []
     #TODO add form validation
+    if params[:email] != params[:confirm_email]
+      @errors << "Emails do no match"
+    end
+    if !params[:agree_to_terms]
+      @errors << "You must accept the terms of agreement"
+    end
+
     #TODO add record creation
+    #
     #TODO add paypal interaction
-    render json: params
+
+    #TODO render success or form with @errors
+    if @errors.length
+      return render "register"
+    end
+    render json: @errors.length > 0 ? @errors : params
   end
 end
