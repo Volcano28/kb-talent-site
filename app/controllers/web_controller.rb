69f0dbc2a3cb8290f3b4dd4291d477b7b61c7caf
Professor Raghunath <raghunat@fredonia.edu>
2015-03-02 20:16:05 -0500

    Added Styles for registration page Added routes for registration page Added registration page view

--- app/controllers/web_controller.rb
+++ app/controllers/web_controller.rb
@@ -1,5 +1,17 @@
 class WebController < ApplicationController
+  protect_from_forgery
   def index
     
+  end
+
+  def register
+    
+  end
+
+  def create_registration
+    #TODO add form validation
+    #TODO add record creation
+    #TODO add paypal interaction
+    render json: params
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'web#index'
+  get 'register', to: 'web#register'
+  post 'register', to: 'web#create_registration'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
